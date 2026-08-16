b2f81939fbf1729ce45a4f2fb34c820a02f09d5b
digihori <yo6987@gmail.com>
2026-08-16 20:50:41 +0900

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0.0"
         buildConfigField("String", "MARKET_API_BASE_URL", "\"https://marketpanel-api.yo6987.workers.dev/\"")
     }
 
@@ -52,6 +52,21 @@ android {
 
     buildFeatures {
         buildConfig = true
+    }
+}
+
+afterEvaluate {
+    val appName =
+        android.defaultConfig.applicationId
+            ?.substringAfterLast(".")
+            ?: "app"
+
+    val version =
+        android.defaultConfig.versionName
+            ?: "0.0.0"
+
+    base {
+        archivesName.set("${appName}-${version}")
     }
 }
 
